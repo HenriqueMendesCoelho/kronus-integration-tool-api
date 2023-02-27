6ddb985ec15504a20b42d11c61c11ac4b0a06fc7
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-02-26 22:22:15 -0300

    fix: include request path logger middleware

--- src/middleware/LoggerMiddleware.ts
+++ src/middleware/LoggerMiddleware.ts
@@ -3,7 +3,9 @@ import { NextFunction, Request, Response } from 'express';
 function logger(request: Request, response: Response, next: NextFunction) {
   const from = request.headers['x-forwarded-for'];
   response.on('finish', () => {
-    console.log(`[${response.statusCode}][${request.method}]/[from:${from}]`);
+    console.log(
+      `[${response.statusCode}][${request.method}]/${request.path}[from:${from}]`
+    );
   });
   next();
 }
