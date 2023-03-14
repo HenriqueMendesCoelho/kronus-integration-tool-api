e37754b42c91745a552683895734135872d98653
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-03-14 11:05:31 -0300

    fix: from logger middleware to get header nginx proxy or remoteAddres

--- src/middleware/LoggerMiddleware.ts
+++ src/middleware/LoggerMiddleware.ts
@@ -2,7 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 import { toISOStringWithTimezone } from '../utils/DateUtils';
 
 function logger(request: Request, response: Response, next: NextFunction) {
-  const from = request.headers['x-forwarded-for'];
+  const from =
+    request.headers['x-forwarded-for'] || request.socket.remoteAddress;
   response.on('finish', () => {
     console.log(
       `${toISOStringWithTimezone(new Date())}[${response.statusCode}][${
