84a3a4135376dc70badaf537d8ad66a0026b0efb
Henrique Mendes <henriquemendescoelho@gmail.com>
2023-05-11 01:42:39 -0300

    fix: add cors config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import { logger } from './middleware/LoggerMiddleware';
 import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(logger);
 app.use(router);
+
+const allowedOrigins = ['*'];
+const corsOpts: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(corsOpts));
 
 export { app };
